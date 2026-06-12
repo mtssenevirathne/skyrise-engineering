3930424b56a0b4155f8e4cb3bdb5848ea792cb7c
mtssenevirathne <senevirathnesandeepa@gmail.com>
2026-06-12 19:56:50 +0530

    update robots sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
     },
 
     sitemap:
-      "https://skyrise-engineering.vercel.app/sitemap.xml",
+      "https://skyriseengineering.lk/sitemap.xml",
   };
 }
